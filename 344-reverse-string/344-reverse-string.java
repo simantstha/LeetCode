ce9dc55e1beb729d3a795eeefa5d5b2a80978418
Simant Shrestha <30699044+simantstha@users.noreply.github.com>
2022-02-25 11:47:27 -0500

    Time: 10 ms (6.59%), Space: 58.2 MB (5.72%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character>stack = new Stack<>();
+        
+        for(char c : s){
+            stack.push(c);
+        }
+        
+        for(int i = 0; i < s.length; i++){
+            s[i] = stack.pop();
+        }
+    }
+}
