9687e2e23ab849277946185ccb1fc19ed5e77790
Simant Shrestha <30699044+simantstha@users.noreply.github.com>
2022-03-30 09:11:06 -0400

    Time: 31 ms (20.77%), Space: 104.1 MB (82.54%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,11 +1,24 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-      for(int i=1;i <= nums.length; i++)
-      {
-        if(Arrays.binarySearch(nums,i) < 0)
-          return i;
-      }
-      return nums.length+1;
+        int max=nums[0];
+        int min=nums[0];
+        HashMap<Integer,Integer> hm= new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+                max=nums[i];
+            if(nums[i]<min)
+                min=nums[i];
+            hm.put(nums[i],1);  
+        }
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(!hm.containsKey(i)&&i!=0)
+            {
+                return i;
+            }
+            
+        }
+        return nums.length+1;
     }
 }
