1a18e46a7dae2a7492afeff3731d99c8bee0693b
Simant Shrestha <30699044+simantstha@users.noreply.github.com>
2022-04-06 09:15:38 -0400

    Time: 3 ms (91.13%), Space: 44.2 MB (52.02%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,24 +1,16 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
-        int n = arr.length;
-        long count = 0;
-        long mod = 1000000007;
-        for(int i = 0; i < n; i++) {
-            //similar to two sum problem solution
-            int new_target = target - arr[i];
-            int start = i + 1;
-            int end = n;
-            HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-            
-            for(int j = start; j < end; j++) {
-                if(hm.get(new_target - arr[j]) != null) 
-                    count += hm.get(new_target - arr[j]);
-                if(hm.get(arr[j]) != null)
-                    hm.put(arr[j], hm.get(arr[j])+1);
-                else
-                    hm.put(arr[j], 1);
+        long res = 0,mod = 1000000007;
+        long[] freq = new long[101];
+        for(int i:arr) freq[i]++;
+        for(int i=0;i<=100;i++)
+            for(int j=i;j<=100;j++){
+                int k = target-i-j;
+                if(k<0 || k>100) continue;
+                if(i == j && j == k) res += (freq[i] * (freq[i]-1) * (freq[i]-2) / 6);
+                else if(i == j && j != k) res += ((freq[i] * (freq[i]-1) / 2) * freq[k]);
+                else if(i < j && j < k) res += freq[i] * freq[j] * freq[k];
             }
-        }
-        return (int)(count % mod);
+        return (int)(res%mod);
     }
-}
+    }
