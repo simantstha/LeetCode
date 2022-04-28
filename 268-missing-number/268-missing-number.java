bf13696cba7b11d8ec1ac6e25c742c7b9bce62df
Simant Shrestha <30699044+simantstha@users.noreply.github.com>
2022-04-28 19:16:48 -0400

    Time: 0 ms (100.00%), Space: 43.1 MB (85.49%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,6 +1,5 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         int sum =0;
         for(int i=0;i<nums.length;i++)
         {
